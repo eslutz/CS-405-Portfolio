a7184a451af05c0767178e58153891dc470e61bc
Eric Slutz <eric.slutz@icloud.com>
2023-07-20 13:27:44 -0400

    completed assignment 4 and updated docs

--- CS405Assignments/Module4/exceptions.cpp
+++ CS405Assignments/Module4/exceptions.cpp
@@ -7,9 +7,17 @@
 
 #include <iostream>
 
+class CustomException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Cusotm exception, a custom exception derived from std::exception has been thrown";
+    }
+};
+
 bool do_even_more_custom_application_logic()
 {
     // TODO: Throw any standard exception
+    throw std::out_of_range("Out of range, throwing any standard C++ exception"); // Throw an out of range exception
 
     std::cout << "Running Even More Custom Application Logic." << std::endl;
 
@@ -18,17 +26,24 @@ bool do_even_more_custom_application_logic()
 void do_custom_application_logic()
 {
     // TODO: Wrap the call to do_even_more_custom_application_logic()
-    //  with an exception handler that catches std::exception, displays
-    //  a message and the exception.what(), then continues processing
-    std::cout << "Running Custom Application Logic." << std::endl;
+    // with an exception handler that catches std::exception, displays
+    // a message and the exception.what(), then continues processing
+    try {
+        std::cout << "Running Custom Application Logic." << std::endl;
 
-    if (do_even_more_custom_application_logic())
-    {
-        std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        if (do_even_more_custom_application_logic())
+        {
+            std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        }
+    }
+    catch (const std::exception& ex) {
+        std::cout << "A standard exception has been caught" << std::endl;
+        std::cout << ex.what() << std::endl;
     }
 
     // TODO: Throw a custom exception derived from std::exception
-    //  and catch it explictly in main
+    // and catch it explictly in main
+    throw CustomException();
 
     std::cout << "Leaving Custom Application Logic." << std::endl;
 
@@ -37,24 +52,31 @@ void do_custom_application_logic()
 float divide(float num, float den)
 {
     // TODO: Throw an exception to deal with divide by zero errors using
-    //  a standard C++ defined exception
+    // a standard C++ defined exception
+    if (den == 0)
+    {
+        throw std::invalid_argument("Invalid argument, cannot divide by zero"); // Throw an invalid argument exception for dividing by zero
+    }
+
     return (num / den);
 }
 
-void do_division() noexcept
-{
+void do_division() noexcept {
     //  TODO: create an exception handler to capture ONLY the exception thrown
     //  by divide.
+    try {
+        float numerator = 10.0f;
+        float denominator = 0;
 
-    float numerator = 10.0f;
-    float denominator = 0;
-
-    auto result = divide(numerator, denominator);
-    std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+        auto result = divide(numerator, denominator);
+        std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+    }
+    catch (const std::invalid_argument& ex) {
+        std::cout << ex.what() << std::endl;
+    }
 }
 
-int main()
-{
+int main() {
     std::cout << "Exceptions Tests!" << std::endl;
 
     // TODO: Create exception handlers that catch (in this order):
@@ -62,8 +84,19 @@ int main()
     //  std::exception
     //  uncaught exception 
     //  that wraps the whole main function, and displays a message to the console.
-    do_division();
-    do_custom_application_logic();
+    try {
+        do_division();
+        do_custom_application_logic();
+    }
+    catch (const CustomException& ex) { // Catch the custom exception derived from the standard exceptions
+        std::cout << ex.what() << std::endl;
+    }
+    catch (const std::exception& ex) { // Catch any standard exception
+        std::cout << ex.what() << std::endl;
+    }
+    catch (...) { // Catch all other types of exceptions
+        std::cout << "An exception has been caught" << std::endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
